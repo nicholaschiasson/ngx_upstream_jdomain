2be0ee95b9f03926241faf6eab077966543de04d
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2020-08-16 18:12:48 -0400

    Support dynamic module

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,16 +4,6 @@ set -ex
 
 source .env
 
-BIN_DIR=${GITHUB_WORKSPACE}/bin
-
-pushd ${BIN_DIR}/workdir
-
-make install
-
-popd
-
-export PATH="${BIN_DIR}/sbin:${PATH}"
-
 echo "nameserver 127.0.0.88" > /etc/resolv.conf
 
 unbound-control -c /tmp/unbound.conf start
@@ -22,4 +12,9 @@ unbound-control verbosity 3
 export TEST_NGINX_USE_VALGRIND=1
 # export TEST_NGINX_CHECK_LEAK=1
 
-prove -rv t/
+DYNAMIC_BIN_DIR=${GITHUB_WORKSPACE}/bin/dynamic
+STATIC_BIN_DIR=${GITHUB_WORKSPACE}/bin/static
+
+# We run prove twice: once with statically linked module and once with dynamically linked module.
+PATH=${STATIC_BIN_DIR}/sbin:${PATH} prove -rv t/
+PATH=${DYNAMIC_BIN_DIR}/sbin:${PATH} TEST_NGINX_LOAD_MODULES=${DYNAMIC_BIN_DIR}/modules/ngx_http_upstream_jdomain_module.so prove -rv t/
