b8cbe6b728d7452a265c3861866e8eadc62fb531
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2020-10-07 23:00:35 -0400

    Some elaborate scripting to guarantee we apply the correct health checks patch

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,41 @@ source .env
 SRC_DIR=/src/nginx
 MOD_DIR=/src/modules
 
+NGINX_VERSION=$(cat ${SRC_DIR}/src/core/nginx.h | grep "#define NGINX_VERSION" | cut -d\" -f2)
+PATCH_VERSION=$(ls ${MOD_DIR}/nginx_upstream_check_module/check_*.patch | sort -Vr | while read f
+do
+	VERS=${f##*_}
+	VERS=${VERS%.*}
+	VERS=${VERS%%+*}
+	NGINX_MAJ=$(echo $NGINX_VERSION | cut -d. -f1)
+	NGINX_MIN=$(echo $NGINX_VERSION | cut -d. -f2)
+	NGINX_PAT=$(echo $NGINX_VERSION | cut -d. -f3)
+	PATCH_MAJ=$(echo $VERS | cut -d. -f1)
+	PATCH_MIN=$(echo $VERS | cut -d. -f2)
+	PATCH_PAT=$(echo $VERS | cut -d. -f3)
+	if [ "$NGINX_MAJ" -gt "$PATCH_MAJ" ]
+	then
+		echo $VERS
+		break
+	elif [ "$NGINX_MAJ" -eq "$PATCH_MAJ" ]
+	then
+		if [ "$NGINX_MIN" -gt "$PATCH_MIN" ]
+		then
+			echo $VERS
+			break
+		elif [ "$NGINX_MIN" -eq "$PATCH_MIN" ]
+		then
+			if [ "$NGINX_PAT" -ge "$PATCH_PAT" ]
+			then
+				echo $VERS
+				break
+			fi
+		fi
+	fi
+done)
+
 pushd ${SRC_DIR}
-patch -p1 < ${MOD_DIR}/nginx_upstream_check_module/check_1.16.1+.patch
+patch -p1 < ${MOD_DIR}/nginx_upstream_check_module/check_${PATCH_VERSION}+.patch
 popd
 
 for type in dynamic static
